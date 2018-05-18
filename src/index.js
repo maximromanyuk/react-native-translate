43a8f34c1fc37a4a61870defc92673cd9bb23250
Nightraiser <nightraiser_official@outlook.com>
2018-05-18 13:51:05 +0530

    When localised keyword doesnt exist in the list, return the original value rather than null (#3)

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import { Text } from 'react-native';
 let _localization = {};
 
 export function setLocalization(localization) {
-    _localization = localization;
+	if(_localization!= undefined){
+		_localization = localization;	
+	}
+    
 }
 
 export function translate(value) {
-    return _localization[value];
+	if(!_localization.hasOwnProperty(value))
+      {
+          return value;
+      }
+      else {
+          return _localization[value];
+      }
 }
 
 export const Translate = ({ value }) => (
